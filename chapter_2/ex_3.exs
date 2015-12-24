8ac177cb7668e421fc2774a5f9d7c0910baaa7e7
Sasha Fonseca <sashaafm@gmail.com>
2015-12-24 23:33:38 +0000

    Fixed exercise 3 to obey the constraints.

--- chapter_2/ex_3.exs
+++ chapter_2/ex_3.exs
@@ -1,4 +1,4 @@
 quote_ = IO.gets(:stdio, "What is the quote?") |> String.replace("\n", "")
 author = IO.gets(:stdio, "Who said it?") |> String.rstrip
 
-~s(#{author} says, \"#{quote_}\") |> IO.puts 
+author <> " says, " <> ~s(\") <> quote_ <> ~s(\") |> IO.puts 
